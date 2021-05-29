8c44cdb3c185d44e196123da515e549b4b1bac2f
hadnet <hadnet1@gmail.com>
2021-05-29 01:32:00 -0300

    fix: fix package.json

--- src/Pagination.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import {render, screen} from '@testing-library/react';
-// import {Pagination} from './Pagination';
-//
-// test('renders learn react link', () => {
-//   render(<Pagination />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
